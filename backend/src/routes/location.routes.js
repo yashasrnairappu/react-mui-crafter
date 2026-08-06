3339b50d95b5f036f2d27bcc3a0ee9a6f62cf46f
yashasrnairappu <yashasrnairappu@gmail.com>
2026-08-06 11:02:39 +0530

    Update location.routes.js

--- backend/src/routes/location.routes.js
+++ backend/src/routes/location.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router()
 import { protectAdmin } from "../middleware/auth.js";
-import { addLocation, city, deleteLocation, getLocations, getSingleLocation, updateLocation ,proxyImage} from '../controllers/location.controller.js';
+import { addLocation, city, district, deleteLocation, getLocations, getSingleLocation, updateLocation ,proxyImage} from '../controllers/location.controller.js';
 import upload from '../middleware/upload.js';
 
 router.post("/addlocations",protectAdmin,upload.array("images", 10),addLocation)
 router.get("/locations", getLocations);
-router.get('/location/cities',city)
+router.get('/location/cities', city)
+router.get('/location/districts', district)
 router.get('/image/:fileId', proxyImage)
 router.get("/location/:id", getSingleLocation);
 router.put("/location/:id", protectAdmin,upload.array("images", 10),updateLocation);
